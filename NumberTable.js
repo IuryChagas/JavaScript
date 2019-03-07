15ff582a2c9aca6936e20115987ed48e77a779a7
Iury Chagas <iurychagas@icloud.com>
2019-03-07 13:51:36 -0300

    tagged template literals

--- NumberTable.js
+++ NumberTable.js
@@ -1,5 +1,6 @@
 let n = 9;
 
 for (let i = 0; i <= 10; i++){
-    console.log(i +" x "+ n +" = "+ i*n);
+    console.log(`${i} x ${n} = ${i*n}`); // Template literals or template strings(ES2015)
+    //console.log(i +" x "+ n +" = "+ i*n);
 }
