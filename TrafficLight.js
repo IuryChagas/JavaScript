be08869e91f4814adbc2d0f2452b38cb5ed13866
Iury Chagas <iurychagas@icloud.com>
2019-03-07 13:13:19 -0300

    traffic light logic conditions (if/ElseIf)

---
+++ TrafficLight.js
@@ -0,0 +1,19 @@
+let light = "blue"; // Change to one of the colors: Green, Yellow or Red!
+
+if (light === "green") {
+    
+    console.log("move on...");
+
+}else if (light === "yellow") {
+
+    console.log("Attention!");
+
+} else if (light === "red") {
+
+    console.log("Stop.");
+
+} else {
+
+    console.log("404 - no answer expected!");
+
+}
