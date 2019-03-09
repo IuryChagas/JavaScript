17b2dbfcdab950255371b3ab75e81add4b47a899
Iury Chagas <iurychagas@icloud.com>
2019-03-09 03:56:20 -0300

    first scripts

---
+++ course4/scripts/calculatorjs.js
@@ -0,0 +1,4 @@
+/**
+ * O arquivo calculatorjs esta apenas representando/instânciando a classe CalcController!
+ */
+window.calculator = new CalcController; // essa variavel calculator é uma nova instância/cópia da classe CalcController
