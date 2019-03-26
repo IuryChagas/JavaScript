134f6f88416d59a0bf77a525831d6663aca8c736
Iury Chagas <iurychagas@icloud.com>
2019-03-26 18:39:19 -0300

    Operators - attribution

---
+++ fundamentals/attribution.js
@@ -0,0 +1,20 @@
+const a = 7;
+let b = 3;
+
+b += a // Isso significa "Atribui√√o aditiva" ou seja, Junto com a atribui√√o, estou fazendo a adi√√o
+       // "Estou acrescendo o valor de "a" na variavel "b".
+       // equivale dizer: b = b + a
+console.log(b)
+
+b -= 4 // Atribui√√o subtrativa, mesmo conceito acima porem subtraindo! equivale: b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+
+b %= 2 // b = b % 2 -- atribui√√o modular "Resto da divis√o" 
+       // Se o resultado for um valor par o resultado sera 0.
+       //  Se o resultado for um valor impar o resultado sera 1.
+
