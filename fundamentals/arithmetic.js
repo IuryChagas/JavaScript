29e473bb5d1cbdfcb091779f4daad0ddeb20847c
Iury Chagas <iurychagas@icloud.com>
2019-03-28 22:40:39 -0300

    operators arithmetic

---
+++ fundamentals/arithmetic.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const sum = a + b + c + d;
+const subtraction = d - b;
+const multiply = a * b;
+const divide = d / a;
+const mod = a % 2; // module
+
+console.log(sum, subtraction, multiply, divide, mod);
