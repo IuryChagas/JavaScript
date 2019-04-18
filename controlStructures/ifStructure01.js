bb5cf7950856f2f46a1cdbe4d22322ae4823614f
iurychagas <iurychagas@icloud.com>
2019-04-18 13:46:30 -0300

    Some boolean tests

--- controlStructures/ifStructure01.js
+++ controlStructures/ifStructure01.js
@@ -11,3 +11,15 @@ function ifTruePrint(value) {
         console.log('It´s true... '+value);
     }
 }
+ifTruePrint(); // turn undefined = False
+ifTruePrint(null); // Null = False
+ifTruePrint(undefined); // explicit undefined = False
+ifTruePrint(NaN); // Not a Number = False
+ifTruePrint(''); // empty string = False
+ifTruePrint(0); // Zero = False
+ifTruePrint(-1); // different from zero = True
+ifTruePrint(' '); // string with blank space = True
+ifTruePrint('?'); // string filled = true
+ifTruePrint([]); // empty array = True
+ifTruePrint([0, 1]); // array with elements = True 
+ifTruePrint({}); //object = True
