efbde91663249872ec0d6e403fa0e7ae1a5f6f9b
Iury Chagas <iurychagas@icloud.com>
2019-10-22 01:54:53 -0300

    add new course5 - via Digital Inovation One

---
+++ course5/scripts/main.js
@@ -0,0 +1,9 @@
+var name = "Iury Chagas";
+var age = 27;
+var phra = "Hellow World";
+
+console.log("initial commit >> "+name);
+console.log(age + " years old");
+console.log(phra);
+console.log(phra.toUpperCase());
+console.log(phra.replace("World", "browser!"));
