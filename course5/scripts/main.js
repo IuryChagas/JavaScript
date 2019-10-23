12e488ab7ead8d8ef620992be42ca011feac585a
Iury Chagas <iurychagas@icloud.com>
2019-10-23 02:39:55 -0300

    condicionais, laços e Date

--- course5/scripts/main.js
+++ course5/scripts/main.js
@@ -1,3 +1,49 @@
+
+var d = new Date();
+
+console.log(d);
+console.log(d.getMinutes());
+console.log(d.getDay());
+console.log(d.getMonth()+1);
+console.log(d.getFullYear());
+console.log(d.getUTCHours());
+console.log(d.getTimezoneOffset());
+
+// https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/73e70077-4510-4e95-87e2-4531514e6c2e
+/*
+
+var age = 27;
+
+if(age >= 18){
+    console.log("Maior de idade!");
+}else if(age <= 17){
+    console.log("Menor de idade!");
+}else{
+    console.log("I don't understand what's that, dude!");
+};
+
+var count = 0;
+while(count < 10){
+    console.log(count);
+    count++;
+    // count = count +1;
+}
+
+console.log("________________________________________");
+for(countf = 10; countf >= 0; countf--){
+    console.log(countf);
+}
+
+
+*/
+
+
+
+
+/** https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/c2f0215d-8643-45ed-a441-ec1669349bca  */
+
+/*
+
 var list = ["apple", "lemon", "orange"];
 console.log(list[0]);
 console.log(list[1]);
@@ -15,6 +61,9 @@ console.log(list.toString()[0]);
 console.log(list.push("watermelon", "apple"));
 console.log(list.join(" | "));
 
+*/
+
+/*
 var fruit = {name: "apple", color: "green"};
 console.log(fruit);
 console.log("fruit name: "+fruit.name);
@@ -24,7 +73,8 @@ var fruits = [{name: "apple", color: "red"}, {name: "orange", color: "orange"}, 
 console.log("fruit name: "+ fruits[1].name);
 console.log("fruit name: "+ fruits[2].name);
 console.log("fruit name: "+ fruits[2].color);
-/*
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 var name = "Iury Chagas";
 var age = 27;
