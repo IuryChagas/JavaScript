d67a9369affdeabd39aad5e8f9dfe1f00f0b3dfb
Iury Chagas <iurychagas@icloud.com>
2019-11-18 00:09:31 -0300

    Basico de var, let, const

---
+++ FullStackJS-2020/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,15 @@
+var a = 4;
+let b = 8;
+const c = 16;
+
+console.log('\nvar = '+ a +'\nlet = '+ b +'\nconst = '+ c);
+
+// palavra reservada variavel pode ser redefinida com novos valores,
+// let não pode ser redefinida assim como const.
+
+
+var a = 42;
+//let b = 12;
+//const c = 2;
+
+console.log('\nNovo valor de var = '+ a +'\n\n');
