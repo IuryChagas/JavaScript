6d7c9b14342f848761728f35a563688451b52587
Iury Chagas <iurychagas@icloud.com>
2020-04-26 13:26:43 -0300

    revisão - Operadores lógicos

---
+++ fundamentals/boolean_logic_op.js
@@ -0,0 +1,11 @@
+console.log(5 > 3 && 3 == 2);
+console.log(27 == 27 && 'iury' == 'iury'); // comparação de tipagem = true. Comparação em valor de string = true. result: true
+console.log(27 == 27 && 'iury' === 'Iury'); // comparação de tipagem = true. Comparação em valor de string = false. result: false
+console.log(typeof 10 == typeof 11); // comparação apenas de tipagem em números.
+console.log("Iury" == "Chagas" || false);
+console.log(!(true && true));
+console.log(true && true);
+console.log(true == true);
+console.log(true === true);
+console.log(false || false);
+console.log(true != true);
