31811627655d0a9e99d5bd45da496fd324b151d8
Iury Chagas <iurychagas@icloud.com>
2020-05-07 23:18:59 -0300

    add SwitchCase

---
+++ fundamentals/switchCase.js
@@ -0,0 +1,12 @@
+var name = "Jessica";
+switch(name) {
+    case "Camilla":
+        console.log("Name: Camilla");
+        break;
+    case "Mariana":
+        console.log("Name: Mariana");
+        break;
+    default:
+        console.log("\n>>> Nome não encontrado na lista!");
+        break;
+}
