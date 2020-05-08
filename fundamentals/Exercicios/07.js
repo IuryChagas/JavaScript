3e135afca2e4c40228422bf3f349108e2c674596
Gomes Das Chagas <dygg@gft.com>
2020-05-08 17:35:48 -0300

    Exercícios [Seção: 5]

---
+++ fundamentals/Exercicios/07.js
@@ -0,0 +1,5 @@
+let age = 17;
+
+if (age >= 18) {
+    console.log('Maior de idade, entrada liberada!');
+}else { console.log('Entrada proibida para menores de 18!');}
---
+++ fundamentals/Exercicios/08.js
@@ -0,0 +1,4 @@
+const name = "Iury";
+if (name == "Iury") {
+    console.log(`\n>>> Olá ${name}, Saudações velho amigo!`);
+}
