a4edc658103201653b79e4623cae7d38c6cd3b19
Iury Chagas <iurychagas@icloud.com>
2020-05-09 17:54:13 -0300

    Escopo aninhado

--- fundamentals/escopo.js
+++ fundamentals/escopo.js
@@ -16,3 +16,21 @@ if(y < 30){
     y *= y;
     console.log(y); // output do escopo local
 }
+
+console.log(`---------`);
+
+let z = 3;
+
+const multiply = function(v0, v1){
+    let z = v0 * v1;
+
+    console.log("output função: "+ z); // escopo da função
+
+    if (z > 10){
+        let z = 350;
+        z++;
+        console.log("output do if: "+ z); // escopo do if
+    }
+}
+console.log("\noutput global: "+ z); // escopo global
+multiply(3, 7); /// escopo da função
