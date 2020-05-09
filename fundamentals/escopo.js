a0fd90eec159095140586ea4bd63f414013966b7
Iury Chagas <iurychagas@icloud.com>
2020-05-09 17:30:25 -0300

    Escopo: let e const

--- fundamentals/escopo.js
+++ fundamentals/escopo.js
@@ -7,3 +7,12 @@ const number = function(){
 
 console.log(n); // n << Escopo Global do script!
 number(n); // n << Escopo Local/'interno' da função!
+
+console.log(`---------`);
+
+const y = 20; // y em escopo global
+if(y < 30){
+    let y = 5; // y em escopo local
+    y *= y;
+    console.log(y); // output do escopo local
+}
