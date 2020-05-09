e5987247b45455d9ac581cea729289204ef40b04
Iury Chagas <iurychagas@icloud.com>
2020-05-09 18:29:53 -0300

    intro Arrow functions

---
+++ fundamentals/arrowFunctions.js
@@ -0,0 +1,21 @@
+const parOuImpar = (value) => {
+    if(value % 2 == 0) {
+       console.log("\n Número informado é: [PAR] ")
+    }else(
+       console.log("\n Número informado é: [IMPAR] ")
+    )
+}
+
+parOuImpar(214);
+
+const outputTest = () => {
+    console.log(" console.log Ok!")
+}
+
+outputTest();
+
+const soma = (a, b) => {
+    return console.log(` ${a + b}`);
+}
+
+soma(500,500);
