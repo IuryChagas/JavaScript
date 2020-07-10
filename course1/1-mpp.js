1e97e97d726db3c0f1bda405ad93cd743a38b0f1
Iury Chagas <iurychagas@icloud.com>
2020-07-10 14:11:06 -0300

    Update Course1 - reviews

---
+++ course1/1-mpp.js
@@ -0,0 +1,18 @@
+console.log("Revisão - 2020");
+
+
+const age = 48;
+const name = "Tião";
+const lastName = "Barreiro";
+let fullName = name + lastName;
+    fullName = name +" "+ lastName;
+
+
+console.log("\n");
+
+console.log(name +" "+ lastName);
+console.log(name, lastName);
+console.log(name +"\n"+ lastName);
+console.log(`${name} ${lastName}`);
+console.log(`${name} \n${lastName}`);
+console.log(fullName);
