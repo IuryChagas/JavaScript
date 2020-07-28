82ed6eb99849b76abd609367c3ef41b470627d3e
Iury Chagas <iurychagas@icloud.com>
2020-07-27 23:50:02 -0300

    add intro to methods

--- orientacao-objetos-intro/index.js
+++ orientacao-objetos-intro/index.js
@@ -1,24 +1,43 @@
 class Cliente { // classes
     nome; // propriedades/atributos
     cpf;
+    // ContaCorrente {}
+};
+
+class ContaCorrente {
     agencia;
     saldo;
-    rg;
-}
+
+    sacar(valor){
+
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+};
 
 const cliente1 = new Cliente();
 
 cliente1.nome = "Ronisclay";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 0001;
-cliente1.saldo = 0;
-cliente1.rg = 1419305426;
 
 const cliente2 = new Cliente();
 
 cliente2.nome = "Gerenilda";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteRonisclay = new ContaCorrente();
+
+contaCorrenteRonisclay.agencia = 0001;
+contaCorrenteRonisclay.saldo = 0;
+
+console.log(contaCorrenteRonisclay.saldo);
+contaCorrenteRonisclay.saldo = 100;
+console.log(contaCorrenteRonisclay.saldo);
+
+contaCorrenteRonisclay.sacar(200);
+console.log(contaCorrenteRonisclay.saldo);
+
+
+console.log(cliente1);
+console.log(cliente2);
