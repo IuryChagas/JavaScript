399ce3c6f5954bde7d42365be03aafccaee7c3f0
Iury Chagas <iurychagas@icloud.com>
2020-08-25 11:04:47 -0300

    add intro js - aula05/ativ07

--- fundamentals/lacosDeRepeticao.js
+++ fundamentals/lacosDeRepeticao.js
@@ -7,21 +7,31 @@ const listaDeDestinos = new Array(
 console.log("Destinos possíveis:\n");
 console.log(listaDeDestinos);
 
-const buyerAge = 23;
+const buyerAge = 21;
 const isAccompanied = false;
-const hasTicket = false;
-const destiny = "Curitiba";
+const hasTicket = true;
+const destiny = "São Paulo";
 let availableDestination = false;
 
 const canBuy = buyerAge >= 18 || isAccompanied == true;
 
-let cont = 0;
-while(cont < listaDeDestinos.length){
-    if (listaDeDestinos[cont] == destiny){
+
+for(let i = 0; i < listaDeDestinos.length; i++){
+    if (listaDeDestinos[i] == destiny) {
         availableDestination = true;
-        break;
     }
-    cont++;
 }
+// while(cont < listaDeDestinos.length){
+//     if (listaDeDestinos[cont] == destiny){
+//         availableDestination = true;
+//         break;
+//     }
+//     cont++;
+// }
 console.log(`Destino disponível: `, availableDestination);
+if (canBuy && availableDestination) {
+    console.log("Venda de ticket para o destino disponível");
+}else{
+    console.log("Desculpe, não foi possíveis prosseguir com a solicitação!")
+}
 console.log(`\n> `+listaDeDestinos);
