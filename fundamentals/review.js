00b0a1cebb3e571fee9155fe2d7d18e8f42745be
Iury Chagas <iurychagas@icloud.com>
2020-08-24 22:43:22 -0300

    add intro js - aula1/ativ03

---
+++ fundamentals/review.js
@@ -0,0 +1,12 @@
+console.log('trabalhando com variáveis');
+
+const idade = 21;
+
+console.log(idade);
+console.log(idade + 2);
+console.log(idade / 2);
+console.log(idade - 2);
+
+const idadeSomada = idade + 4;
+
+console.log(idadeSomada);
