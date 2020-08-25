a2768b962ee91b67379b0d9b1e3df8fbe78c1d8e
Iury Chagas <iurychagas@icloud.com>
2020-08-24 23:37:17 -0300

    add intro js - aula3/ativ2

---
+++ fundamentals/lists.js
@@ -0,0 +1,20 @@
+console.log("## Trabalhando com listas ##\n");
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+// console.log("Destinos possíveis:\n");
+// console.log(salvador, saoPaulo, rioDeJaneiro);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+console.log("Destinos possíveis:\n");
+console.log(listaDeDestinos);
+
+listaDeDestinos.push('Curitiba');
+console.log(listaDeDestinos);
