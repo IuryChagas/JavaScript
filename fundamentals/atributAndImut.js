eaa7f1f683a84690bf6fcac6697d07305e424edd
Iury Chagas <iurychagas@icloud.com>
2020-08-24 23:25:18 -0300

    add intro js aula2/ativ05

---
+++ fundamentals/atributAndImut.js
@@ -0,0 +1,14 @@
+let name = 'iury';
+const nickname = 'chagas';
+
+console.log(name + nickname);
+console.log(name, nickname);
+console.log(`${name} ${nickname}`);
+console.log(name+ ' ' +nickname);
+
+name = name + " " + nickname;
+console.log(name);
+
+const fullName = name;
+
+console.log(fullName);
