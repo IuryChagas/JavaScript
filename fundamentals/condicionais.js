ef30cda06f9bac978015d82923fb6cb440dd93ca
Iury Chagas <iurychagas@icloud.com>
2020-08-25 00:22:37 -0300

    add intro js - aula4/ativ03

--- fundamentals/condicionais.js
+++ fundamentals/condicionais.js
@@ -7,14 +7,17 @@ const listaDeDestinos = new Array(
 console.log("Destinos possíveis:\n");
 console.log(listaDeDestinos);
 
-const buyerAge = 112;
+const buyerAge = 16;
+isAccompanied = true;
+
 if(buyerAge >= 18){
     console.log('Comprador maior de idade');
-            // à partir da posição 1, delete 1 elemento!
-    listaDeDestinos.splice(1,1); // remove item
-}else {
+    listaDeDestinos.splice(1,1);
+}else if(isAccompanied){
+    console.log('Comprador está acompanhado!');
+    listaDeDestinos.splice(1,1);
+}else{
     console.log("Não é maior de idade e não é possível liberar a venda!")
 }
-
 
 console.log(`\n> `+listaDeDestinos);
