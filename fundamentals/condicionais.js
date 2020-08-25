2ae60278bdc462d1efbb0d51a00371aa034a279d
Iury Chagas <iurychagas@icloud.com>
2020-08-25 00:38:42 -0300

    add intro js - aula4/ativ05

--- fundamentals/condicionais.js
+++ fundamentals/condicionais.js
@@ -7,17 +7,22 @@ const listaDeDestinos = new Array(
 console.log("Destinos possíveis:\n");
 console.log(listaDeDestinos);
 
-const buyerAge = 16;
-isAccompanied = true;
+const buyerAge = 23;
+const isAccompanied = false;
+const hasTicket = false;
 
-if(buyerAge >= 18){
-    console.log('Comprador maior de idade');
-    listaDeDestinos.splice(1,1);
-}else if(isAccompanied){
-    console.log('Comprador está acompanhado!');
-    listaDeDestinos.splice(1,1);
+if(buyerAge >= 18 || isAccompanied){
+    console.log('Venda liberada!');
+    listaDeDestinos.splice(2,1);
 }else{
-    console.log("Não é maior de idade e não é possível liberar a venda!")
+    console.log("Não é maior de idade, não é possível liberar a venda!")
 }
 
+console.log('Embarque: \n\n');
+
+if(isAccompanied || buyerAge >=18 && hasTicket){
+    console.log('Embarque liberado... - Boa viagem!');
+}else {
+    console.log("Embarque bloqueado!");
+}
 console.log(`\n> `+listaDeDestinos);
