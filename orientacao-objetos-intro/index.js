ea946602a32d6114e0cf479833059a33654d03d4
Iury Chagas <iurychagas@icloud.com>
2020-08-26 01:13:26 -0300

    add initial customer class

--- orientacao-objetos-intro/index.js
+++ orientacao-objetos-intro/index.js
@@ -1,4 +1,24 @@
-const name;
-const cpf;
-const agency;
-const balance;
+class customer {
+
+    name;
+    cpf;
+    agency;
+    balance;
+
+}
+
+const client1 = new customer();
+
+client1.name = 'Iuzy';
+client1.cpf = 91468194011;
+client1.agency = 1001;
+client1.balance = 0;
+
+const client2 = new customer();
+
+client2.name = 'Douglas';
+client2.cpf = 32987705158;
+client2.agency = 1001;
+client2.balance = 0;
+
+console.log(client1);
