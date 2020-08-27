120cc9556d99f79ce541aedc351f71e1f7f67bc7
Iury Chagas <iurychagas@icloud.com>
2020-08-26 23:02:18 -0300

    add private fields

--- orientacao-objetos-intro/index.js
+++ orientacao-objetos-intro/index.js
@@ -5,14 +5,23 @@ class customer {
 
 class checkingAccount {
     agency;
-    balance;
+    _balance = 0; // #private fields:   https://github.com/tc39/proposal-class-fields#private-fields 
 
     withdraw(value){
-        if (this.balance >= value) {
-            this.balance -= value;
+        if (this._balance >= value) {
+            this._balance -= value;
             console.log("withdrawal value: ", value);
         }else{
             console.log("> Insufficient balance, Invalid operation!");
+        }
+    }
+    deposit(value){
+        if(value >= 1){
+            this._balance += value;
+            console.log("deposit value: ", value);
+            console.log(`total balance: R$ ${this._balance}`);
+        }else {
+            console.log("Invalid operation!");
         }
     }
 }
@@ -29,18 +38,19 @@ client2.cpf = 32987705158;
 
 const checkingAccountIuzy = new checkingAccount();
 checkingAccountIuzy.agency = 1001;
-checkingAccountIuzy.balance = 0;
 
 const checkingAccountDouglas = new checkingAccount();
 checkingAccountDouglas.agency = 1001;
-checkingAccountDouglas.balance = 0;
 
-console.log(client1, checkingAccountIuzy);
-console.log(client2, checkingAccountDouglas);
-
-checkingAccountDouglas.balance = 100;
+checkingAccountDouglas.deposit(100);
 console.log(client2, checkingAccountDouglas);
 
 checkingAccountDouglas.withdraw(50);
 
 console.log(client2, checkingAccountDouglas);
+
+checkingAccountDouglas.deposit(3000);
+
+console.log(checkingAccountDouglas);
+
+console.log(checkingAccountDouglas);
