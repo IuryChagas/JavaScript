b7da72eccbc613150dee023c5013ddc921ed68a7
Iury Chagas <iurychagas@icloud.com>
2020-08-28 16:01:59 -0300

    add index update with constructors adjusts

--- orientacao-objetos-intro/index.js
+++ orientacao-objetos-intro/index.js
@@ -1,29 +1,16 @@
 import {Customer} from "./Customer.js";
 import {CheckingAccount} from "./CheckingAccount.js";
 
-const client1 = new Customer();
-client1.name = 'Iuzy';
-client1.cpf = 91468194011;
+const client1 = new Customer('Iuzy', 91468194011);
+const client2 = new Customer('Douglas', 32987705158);
 
-const client2 = new Customer();
-client2.name = 'Douglas';
-client2.cpf = 32987705158;
-
-const checkingAccountIuzy = new CheckingAccount();
-checkingAccountIuzy.agency = 1001;
-checkingAccountIuzy.client = client1;
+const checkingAccountIuzy = new CheckingAccount(1001, client1);
 checkingAccountIuzy.deposit(500);
 
-const checkingAccountDouglas = new CheckingAccount();
-checkingAccountDouglas.agency = 1001;
-checkingAccountDouglas.client = client2;
-
+const checkingAccountDouglas = new CheckingAccount(1001, client2);
 checkingAccountIuzy.transfer(200, checkingAccountDouglas);
 
-console.log(checkingAccountIuzy);
-console.log(checkingAccountDouglas);
-console.log(checkingAccountIuzy.balance);
-console.log(checkingAccountIuzy.client, checkingAccountDouglas.client);
-
+console.log(checkingAccountIuzy, checkingAccountDouglas);
+console.log("CPF: ", client2.cpf);
 
 
