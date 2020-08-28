3e25034e0fed184c615765004d5f995e9d21f0ff
Iury Chagas <iurychagas@icloud.com>
2020-08-28 01:18:33 -0300

    add about Null x undefined

--- orientacao-objetos-intro/index.js
+++ orientacao-objetos-intro/index.js
@@ -1,23 +1,35 @@
 import {Customer} from "./Customer.js";
 import {CheckingAccount} from "./CheckingAccount.js";
 
-const client1 = new Customer();
-client1.name = 'Iuzy';
-client1.cpf = 91468194011;
-
 const checkingAccountIuzy = new CheckingAccount();
 checkingAccountIuzy.agency = 1001;
-checkingAccountIuzy.client = client1;
+checkingAccountIuzy.client = new Customer();
+checkingAccountIuzy.client.name = 'Iuzy';
+checkingAccountIuzy.client.cpf = 91468194011;
 checkingAccountIuzy.deposit(500);
-
-const client2 = new Customer();
-client2.name = 'Douglas';
-client2.cpf = 32987705158;
 
 const checkingAccountDouglas = new CheckingAccount();
 checkingAccountDouglas.agency = 1001;
-checkingAccountDouglas.client = client2;
+checkingAccountDouglas.client = new Customer();
+checkingAccountDouglas.client.name = 'Douglas';
+checkingAccountDouglas.client.cpf = 32987705158;
+
 checkingAccountIuzy.transfer(200, checkingAccountDouglas);
 
 console.log(checkingAccountIuzy);
 console.log(checkingAccountDouglas);
+
+const checkingAccountChagas = new CheckingAccount();
+checkingAccountChagas.agency = 1003;
+checkingAccountChagas.client = null;
+
+// nulll - manually configured
+console.log(checkingAccountChagas);
+
+const checkingAccountIury = new CheckingAccount();
+checkingAccountIury.agency = 1003;
+checkingAccountIury.client.name = 'Iury';
+
+// undefined - by default
+console.log(checkingAccountIury);
+
