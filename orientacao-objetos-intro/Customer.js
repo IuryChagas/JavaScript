61a40de453c752fcd9e378934ae10343b7b81d55
Iury Chagas <iurychagas@icloud.com>
2020-08-28 15:54:10 -0300

    add Customer constructors

--- orientacao-objetos-intro/Customer.js
+++ orientacao-objetos-intro/Customer.js
@@ -1,4 +1,12 @@
 export class Customer{
-    name;
-    cpf;
+    _name;
+    _cpf;
+
+    get name() { return this._name; }
+    get cpf(){ return this._cpf; }
+
+     constructor(name, cpf){
+        this._name = name;
+        this._cpf = cpf;   
+    }
 };
