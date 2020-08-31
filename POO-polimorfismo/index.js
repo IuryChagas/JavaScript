4cb944858ab16e76cddde5da4d3423dc5bc663cf
Iury Chagas <iurychagas@icloud.com>
2020-08-31 16:54:38 -0300

    Update index

--- POO-polimorfismo/index.js
+++ POO-polimorfismo/index.js
@@ -1,16 +1,14 @@
 import {Customer} from "./Customer.js";
+import {Account} from "./Account.js";
 import {CheckingAccount} from "./CheckingAccount.js";
+import {SavingsAccount} from "./SavingsAccount.js";
 
-const client1 = new Customer('Iuzy', 91468194011);
-const client2 = new Customer('Douglas', 32987705158);
+const client1 = new Customer('Iury', 32987705158);
 
-const checkingAccountIuzy = new CheckingAccount(1001, client1);
-checkingAccountIuzy.deposit(500);
+const checkingAccountDouglas = new CheckingAccount(0, client1, 1001);
+checkingAccountDouglas.deposit(500);
 
-const checkingAccountDouglas = new CheckingAccount(1001, client2);
-checkingAccountIuzy.transfer(200, checkingAccountDouglas);
-
-console.log(checkingAccountIuzy);
 console.log(checkingAccountDouglas);
 
-console.log("Accounts number: ", CheckingAccount.accountsCounter);
+const savingsAccount = new SavingsAccount(50, client1, 1001);
+console.log(savingsAccount);
