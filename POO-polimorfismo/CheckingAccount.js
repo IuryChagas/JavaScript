e0e23d0ef83b1309d88c3620467c89a132a73cf1
Iury Chagas <iurychagas@icloud.com>
2020-08-31 13:50:20 -0300

    add starting course 2 - Polymorphism
    Continuação do curso introdução à orientação de objetos

---
+++ POO-polimorfismo/CheckingAccount.js
@@ -0,0 +1,37 @@
+import { Customer } from "./Customer.js";
+export class CheckingAccount {
+    static accountsCounter = 0;
+    agency;
+    _client;
+
+    set client(value){
+        if (value instanceof Customer) {
+            this._client = value;   
+        }
+    }
+    get client(){
+        return this._client;
+    }
+    _balance = 0; 
+
+    get balance(){
+        return this._balance;
+    }
+    constructor(agency, client){
+        this.agency = agency;
+        this.client = client;
+        CheckingAccount.accountsCounter++;
+    }
+    withdraw(value){
+        if (value > this._balance || value <= 0) return;
+        this._balance -= value;
+    }
+    deposit(value){
+        if(value <= 0) return;
+        this._balance += value;
+    }
+    transfer(value, checkingAccount){
+        const withdrawal = this.withdraw(value);
+        checkingAccount.deposit(value);
+    }
+};
