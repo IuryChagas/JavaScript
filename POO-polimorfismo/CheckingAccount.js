900f220aed794b79994bb7b37d5b524f1c163cb2
Iury Chagas <iurychagas@icloud.com>
2020-08-31 14:07:00 -0300

    Update Instance properties - CheckingAccount
    Instance properties must be defined inside of class methods:

--- POO-polimorfismo/CheckingAccount.js
+++ POO-polimorfismo/CheckingAccount.js
@@ -1,9 +1,6 @@
 import { Customer } from "./Customer.js";
 export class CheckingAccount {
     static accountsCounter = 0;
-    agency;
-    _client;
-
     set client(value){
         if (value instanceof Customer) {
             this._client = value;   
@@ -12,14 +9,13 @@ export class CheckingAccount {
     get client(){
         return this._client;
     }
-    _balance = 0; 
-
     get balance(){
         return this._balance;
     }
     constructor(agency, client){
         this.agency = agency;
         this.client = client;
+        this._balance = 0;
         CheckingAccount.accountsCounter++;
     }
     withdraw(value){
