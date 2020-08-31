e506b2a2630f56949ba7d0d4ea4efe9edc3183f7
Iury Chagas <iurychagas@icloud.com>
2020-08-31 17:08:27 -0300

    fix constructor of checking Account

--- POO-polimorfismo/CheckingAccount.js
+++ POO-polimorfismo/CheckingAccount.js
@@ -3,7 +3,7 @@ import { Account } from "./Account.js";
 export class CheckingAccount extends Account{
     static accountsCounter = 0;
 
-    constructor(client, agency){
+    constructor(balance, client, agency){
         super(0, client, agency);
         CheckingAccount.accountsCounter++;
     }
