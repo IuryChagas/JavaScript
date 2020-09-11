b1e0485c0dde0eaa45c2638b78fe012c00ca7c5a
Iury Chagas <iurychagas@icloud.com>
2020-09-11 17:14:31 -0300

    functions Exercises

---
+++ fundamentals/Exercicios/functionsExercises.js
@@ -0,0 +1,9 @@
+function Nrunner(num){
+    return Math.floor(Math.random() * num)+1;
+}
+
+console.log("max  (10): ", Nrunner(10));
+console.log("max (100): ", Nrunner(100));
+console.log("max:  (4): ", Nrunner(4));
+console.log("max: (15): ", Nrunner(15));
+
