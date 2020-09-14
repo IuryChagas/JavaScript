bd74a8140c2f765568066c91f54acda7cd666c70
Iury Chagas <iurychagas@icloud.com>
2020-09-14 16:43:32 -0300

    add about object and arrays introduction

---
+++ fundamentals/Exercicios/objectsAndArrays.js
@@ -0,0 +1,12 @@
+let numbers = [22, 35, 29, 1, 9];
+let informations = ["Iury", 12, true, 2, "Last value of array!"];
+let names = ["Chagas", "iury", "Madaline", "Camilla"];
+
+console.log(numbers);
+console.log(informations);
+console.log(names);
+
+console.log("\nName: ", names[3]);
+console.log(">>", informations[informations.length -1]);
+console.log(">>", informations[informations.length -3]);
+console.log(">>", typeof numbers);
