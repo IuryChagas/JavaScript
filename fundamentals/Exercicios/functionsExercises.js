9ff0689697df4509e52c4abdbba263fd0a887014
Iury Chagas <iurychagas@icloud.com>
2020-09-14 16:42:10 -0300

    add about select and extract Even number

--- fundamentals/Exercicios/functionsExercises.js
+++ fundamentals/Exercicios/functionsExercises.js
@@ -1,11 +1,9 @@
-function exponentialFunction(baseNumber, ExponentNumber){
-    return console.log("\n>> result: ", baseNumber ** ExponentNumber)
+function extractEvenNumbers(number){
+    for (let n = 0; n < number;) {
+            if (number % 2 == 0) {
+                console.log(number);
+            }
+            number--;
+    }
 }
-exponentialFunction(2, 2);
-exponentialFunction(4,4);
-
-function potencia(baseNumber, ExponentNumber){
-    return Math.pow(baseNumber, ExponentNumber)
-}
-console.log("\n>> result: ",potencia(3,2));
-console.log("\n>> result: ",potencia(5,2));
+extractEvenNumbers(15);
