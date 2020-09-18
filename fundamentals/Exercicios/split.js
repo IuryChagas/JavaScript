bbf8df9955d3e5176ab05a471aa07062bef6672a
Iury Chagas <iurychagas@icloud.com>
2020-09-17 22:38:11 -0300

    add about convert string to array

---
+++ fundamentals/Exercicios/split.js
@@ -0,0 +1,9 @@
+let phrase = "The rat chewed the cork of the bottle of the King of Russia.";
+
+console.log('Array items: ', phrase.split(" "));
+console.log('Characters numbers:',phrase.split(" ").length);
+console.log('Array to string conversion: ', phrase.split(" ").toString());
+console.log('Length number of string: ', phrase.split(" ").toString().length);
+
+let fruits = "Grapes;Kiwifruit;Acerola;Clementine;Figs;Watermelon;Banana;Durin";
+console.log(fruits.split(";"));
