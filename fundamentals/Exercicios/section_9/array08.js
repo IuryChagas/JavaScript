db4ff25178b0774b98c437563bf0087a084c0168
Iury Chagas <iurychagas@icloud.com>
2020-09-22 15:18:25 -0300

    add exerc08 - JSON

---
+++ fundamentals/Exercicios/section_9/array08.js
@@ -0,0 +1,11 @@
+let dogs = {
+    "dog1": "French Bulldogs", 
+    "dog2": "Bulldogs",
+    "dog3": "Beagles",
+};
+
+console.log();
+console.log('Type of:',typeof dogs);
+console.log('\n>>', dogs.dog1);
+console.log('>>', dogs.dog2);
+console.log('>>', dogs.dog3);
