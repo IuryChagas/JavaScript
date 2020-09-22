c11baa288d098965353411565ed41e85d04e4d2d
Iury Chagas <iurychagas@icloud.com>
2020-09-22 00:04:24 -0300

    add exerc03 - objs

---
+++ fundamentals/Exercicios/section_9/array03.js
@@ -0,0 +1,9 @@
+const bus = {
+    wheels: 8,
+    passengers: 40,
+    doors: 2
+};
+
+console.log("\nwheels: ", bus.wheels);
+console.log("passengers: ", bus.passengers);
+console.log("doors: ", bus.doors);
