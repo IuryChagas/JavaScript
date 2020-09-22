9492b30eb4495f6afa866b4c2f331ca47cc84b76
Iury Chagas <iurychagas@icloud.com>
2020-09-21 23:52:11 -0300

    add exerc02 - arrays

---
+++ fundamentals/Exercicios/section_9/array02.js
@@ -0,0 +1,6 @@
+const year = [2020,2017];
+
+const month = ['january', 'october', 'august', 'july'];
+
+console.log('N° items > Year:', year.length);
+console.log('N° items > Month:', month.length);
