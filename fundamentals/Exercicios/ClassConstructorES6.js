6e3ba3f3bb723ade19b98e37dc5f1d40b2e4d3ea
Iury Chagas <iurychagas@icloud.com>
2020-09-25 00:08:11 -0300

    add ES6 Class and Constructor

---
+++ fundamentals/Exercicios/ClassConstructorES6.js
@@ -0,0 +1,15 @@
+class Cachorro {
+    constructor(raca, patas, cor){
+        this.raca = raca;
+        this.patas = patas;
+        this.cor = cor;
+    };
+};
+
+let labrador = new Cachorro('labrador', 4, 'Caramelo');
+
+
+console.log('\n',labrador);
+console.log('\n Raça:',labrador.raca);
+
+// caniuse.com
