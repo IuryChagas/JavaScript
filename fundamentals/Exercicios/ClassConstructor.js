3ebafa576045393d988037f2f1b0dd6ac4ad4bb1
Iury Chagas <iurychagas@icloud.com>
2020-09-24 23:35:32 -0300

    add Class constructor with function()

---
+++ fundamentals/Exercicios/ClassConstructor.js
@@ -0,0 +1,20 @@
+// Construtores são formas de instânciar uma classe, ou seja é o mesmo de criar 'instanciar' um objeto.
+// No Construtor já podemos definir propriedades;
+
+function createGuitar(body, strings, bridge){
+    let guitar = Object.create({});
+    guitar.body = body;
+    guitar.strings = strings;
+    guitar.bridge = bridge;
+    guitar.timbre = function(){
+        console.log('\nTimbre Clean: ♫ ♩ ♫ ♪ ♫ ♪!');
+    }
+
+    return guitar;
+};
+
+let stratoCaster = createGuitar('wood', 7, 'bronze');
+
+console.log(stratoCaster);
+
+stratoCaster.timbre();
