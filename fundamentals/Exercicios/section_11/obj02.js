eca3fb06cf432ca7b5932dca432bdcb80f1ea3c1
Iury Chagas <iurychagas@icloud.com>
2020-10-01 23:47:29 -0300

    add about exerc37 - 'something's wrong'

--- fundamentals/Exercicios/section_11/obj02.js
+++ fundamentals/Exercicios/section_11/obj02.js
@@ -1,39 +1,68 @@
 class Cart {
-    constructor(item, value, quantity){
-        if (item === undefined && value === undefined && quantity === undefined) {
-        item = '';
-        value = 0;
-        quantity = 0;
+    constructor(items, quantity, valueTotal){
+        if (items === undefined && valueTotal === undefined && quantity === undefined) {
+        items = items;
+        quantity = quantity;
+        valueTotal = valueTotal;
     }
         this.id = 1;
-        this.items = item;
-        this.value = value;
+        this.items = items;
         this.quantity = quantity;
+        this.valueTotal = valueTotal;
     }
-    addItem(item, value, quantity){
-        this.items += item;
-        this.value += value;
-        this.quantity += quantity;
+    addItem(item){
+        let count = 0;
+        for (let cartItem = 0; cartItem < this.items; cartItem) {
+            if (this.items[cartItem].id == item.id) {
+                this.items[cartItem].quantity += item.quantity;
+                count = 1;
+            }
+        }
+        if (count == 0) {
+            this.items.push(item);
+        }
+        this.quantity += item.quantity;
+        this.valueTotal += item.value * item.quantity;
     }
-    delItem(id){
-        this.id = undefined;
-        this.items = undefined;
-        this.value = undefined;
-        this.quantity = undefined;
-        console.log('\nexcluded with success!\n')
+
+    removeItem(){
+        for (let cartItem = 0; cartItem < this.items.length; cartItem++) {
+            if (this.items[cartItem] == cartItem.id) {
+                let obj = this.item[cartItem];
+                let index = this.item.findIndex(function(obj){
+                    return obj.id == item.id;
+                })
+                this.valueTotal -= this.items[cartItem].quantity;
+                this.valueTotal -= this.items[cartItem].value * this.items[cartItem].quantity;
+
+                this.items.split(index, 1);
+            }    
+        }
     }
 }
 
-let book = new Cart()
-    console.log(book)
+let cart = new Cart([
+    {
+        id: 01,
+        item: 'Book',
+        quantity: 3,
+        value: 183.73
+    },
+    {
+        id: 02,
+        item: 'Clook',
+        quantity: 1,
+        value: 45.96
+    }
+], 4, 229.69)
 
-let clock = new Cart('clock', 173.15, 15)
-    console.log(clock)
+console.log(cart)
 
-clock.delItem(1);
-    console.log(clock)
+cart.addItem({id:03, item:"Book", quantity: 2, value: 36.90})
+    console.log('>> Cart quantity:', cart.quantity, '\n>> Value total:', cart.valueTotal)
 
-book.addItem('outliers', 167.83, 2)
-    console.log(book)
+cart.addItem({id:04, item:"backpack", quantity: 1, value: 941.72})
+    console.log(cart)
 
-console.log(book.value)
+cart.removeItem({id:03, item:"Book", quantity:2, value: 36.90});
+    console.log(cart)
