41271dc6d2187f21ef8fdf0a9a7de59ada54c783
Iury Chagas <iurychagas@icloud.com>
2020-10-28 00:36:16 -0300

    add more about moving through the DOM

--- fundamentals/javaScript_onBrowser/scripts.js
+++ fundamentals/javaScript_onBrowser/scripts.js
@@ -1,7 +1,9 @@
 console.log(document.body)
 console.log(document.body.childNodes)
-console.log(document.body.childNodes[0])
+console.log(document.body.childNodes[3].innerText)
 console.log(document.body.childNodes[1])
 console.log(document.body.childNodes[1].childNodes[1])
 console.log(document.body.childNodes[1].childNodes[1].childNodes[0])
 console.log(document.body.childNodes[1].childNodes[1].textContent)
+
+console.log(document.body.childNodes[1].outerText)
