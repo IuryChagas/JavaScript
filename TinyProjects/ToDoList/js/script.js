a8302be590a76bae900541e101908dab82f98456
Iury Chagas <iurychagas@icloud.com>
2021-01-22 14:22:20 -0300

    add addTask() method
    finalizado método p/ add novas tarefas, instânciado classe e add método
    p/ desviar envio padrão no formulário.

---
+++ TinyProjects/ToDoList/js/script.js
@@ -0,0 +1,36 @@
+class Todo {
+    constructor(){
+        this.totalTasks = document.querySelectorAll('.task').length
+    }
+    addTask(taskText){
+
+        let template = document.querySelector('.task').cloneNode(true);
+
+        template.classList.remove('hide')
+
+        let templateText = template.querySelector('.task-title')
+
+        templateText.textContent = taskText;
+
+        let tasksList = document.querySelector('#tasks-container')
+
+        tasksList.appendChild(template)
+
+    }
+}
+
+let todo = new Todo()
+
+let addBtn = document.querySelector('#add')
+
+addBtn.addEventListener('click', function(event){
+
+    event.preventDefault();
+
+    let taskText = document.querySelector('#task')
+
+    todo.addTask(taskText.value)
+
+    taskText.value = '';  
+
+})
