91f56527269036bd09d07631ea9d132052ee349c
Iury Chagas <iurychagas@icloud.com>
2021-03-12 00:26:52 -0300

    Add 'exercise 11' practices

---
+++ CJRM/exercicio-11/app.js
@@ -0,0 +1,103 @@
+/*
+  01
+
+  - Declare uma constante que recebe o seu nome;
+  - Ela deve ter um escopo global.
+*/
+console.info('\n### Etapa 04 - Exercício: 01 ###\n ')
+
+const name = 'iury'
+/*
+  02
+
+  - Funções também são blocos de código;
+  - Crie uma função que ao ser invocada, exibe sua idade no console;
+  - Dentro da função, declare uma let que armazena a sua idade;
+  - Exiba sua idade no console, através da invocação da função;
+  - Do lado de fora da função exiba no console o valor da let que foi declarada  
+    dentro da função;
+  - Um erro com a mensagem "Uncaught ReferenceError: SUA_VARIÁVEL is not  
+    defined" será exibido no console;
+  - Você sabe por que isso aconteceu?
+*/
+console.info('\n### Etapa 04 - Exercício: 02 ###\n ')
+
+function getAge() {
+  let age = 49
+  console.log(`age: `, age)
+}
+
+getAge()
+
+console.log(`Este erro ocorre pq a variável AGE, não existe no escopo ao qual esta sendo invocada!`)
+// console.log(age)
+
+/*
+  03
+
+  - Crie um objeto "car" com as seguintes propriedades e métodos:
+    - name, que recebe o nome do carro;
+    - brand, que recebe a marca do carro;
+    - colors, que recebe 3 cores para o carro;
+    - isRunning, que recebe um boolean indicando se o carro está em movimento.  
+      Inicialmente, deve receber um boolean indicando que o carro não está em  
+      movimento;
+    - run, que é um método que faz o carro andar e retorna a mensagem  
+      "O NOME_DO_CARRO está em movimento";
+    - stop, que é um método que faz o carro parar e retorna a mensagem  
+      "O NOME_DO_CARRO está parado";
+    - getColorsMessage, que é um método que retorna a mensagem  
+      "O NOME_DO_CARRO está disponível nas cores COR_01, COR_02 e COR_03".
+*/
+console.info('\n### Etapa 04 - Exercício: 03 ###\n ')
+
+let car = {
+  name: 'Model S',
+  brand: 'Tesla',
+  colors: ['Grey', 'Black', 'Red'],
+  isRunning: false,
+  run(){
+    console.log(`O ${this.name} está em movimento...`)
+  },
+  stop(){
+    console.log(`O ${this.name} está parado!`)
+  },
+  getColorsMessage(){
+    console.log(`O ${this.name} está disponível nas cores ${this.colors[0]}, ${this.colors[1]} e ${this.colors[2]}`)
+  }
+}
+
+console.log(car)
+/*
+  04
+
+  - Faça o carro andar e exiba no console se ele realmente está em movimento.
+*/
+console.info('\n### Etapa 04 - Exercício: 04 ###\n ')
+
+car.run()
+/*
+  05
+
+  - Faça o carro parar e exiba no console se ele realmente está parado.
+*/
+console.info('\n### Etapa 04 - Exercício: 05 ###\n ')
+
+car.stop()
+/*
+  06
+
+  - Exiba, no console, a mensagem com as cores do carro.
+*/
+console.info('\n### Etapa 04 - Exercício: 06 ###\n ')
+
+car.getColorsMessage()
+/*
+  07
+
+  - Exiba, no console, a mensagem "O carro é um MARCA_DO_CARRO NOME_DO_CARRO";
+  - Utilize a notação de colchetes para acessar as propriedades do carro.
+*/
+console.info('\n### Etapa 04 - Exercício: 07 ###\n ')
+
+console.log(`O carro é um ${car[`brand`]} ${car[`name`]}`)
