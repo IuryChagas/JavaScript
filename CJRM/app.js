720dddd949fe7a07e85abc233ce4fad0d08f39bf
Iury Chagas <iurychagas@icloud.com>
2021-04-07 09:58:45 -0300

    Add about 'arrays chaining'

--- CJRM/app.js
+++ CJRM/app.js
@@ -1,35 +1,15 @@
-// Exemplo 1: Ordenando por strings
-const names = ['Christian','Alfredo', 'Beatriz', 'Edson']
-
-names.sort()
-
-console.log(names)
-
-// Exemplo 2: Ordenando Objetos
-
-const theBigFamily = [
-    { name: 'Lineu', score: 20 },
-    { name: 'Nenê', score: 10 },
-    { name: 'Tuco', score: 50 },
-    { name: 'Bebel', score: 30 },
-    { name: 'Agostinho', score: 70 }
-]
-
-theBigFamily.sort((item1, item2) => {
-    if (item1.score > item2.score) {
-        return -1
-    }else if (item2.score > item1.score) {
-        return 1
-    }
-    return 0
-})
-
-console.log(theBigFamily)
+const books = [
+    { name: 'Código Limpo', price: 30 },
+    { name: 'O milagre da manhã', price: 5 },
+    { name: 'Alice no País das Maravilhas', price: 10 },
+    { name: 'Quem Pensa Enriquece', price: 50 },
+    { name: 'O livro da ciência', price: 40 }
+  ]
 
 
-// Exemplo 3: Ordenando números
-const scores = [10, 50, 20, 5, 35, 70, 45]
+const booksOnSale = books
+    .filter( ({ price }) =>  price > 20)
+    .map(({name, price}) =>
+        `O preço do livro "${name}" caiu para ${price}\n`)
 
-scores.sort((score1, score2) => score1 - score2)
-
-console.log(scores)
+console.log(booksOnSale)
