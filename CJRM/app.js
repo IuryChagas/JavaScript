fafa5bd43989de7330fec8ed86cccf789dee1c04
Iury Chagas <iurychagas@icloud.com>
2021-04-15 00:48:03 -0300

    Add To-Do --searching and filtering items

--- CJRM/app.js
+++ CJRM/app.js
@@ -1,6 +1,6 @@
 const todosContainer = document.querySelector('.todos-container')
 const formAddTodo = document.querySelector('.form-add-todo')
-
+const inputSearchTodo = document.querySelector('.form-search input') 
 
 formAddTodo.addEventListener('submit', event => {
   event.preventDefault()
@@ -26,4 +26,20 @@ todosContainer.addEventListener('click', event => {
     clickedElement.parentElement.remove()
   }
 
+})
+
+inputSearchTodo.addEventListener('input', event => {
+  const inputValue = event.target.value.trim().toLowerCase()
+  Array.from(todosContainer.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+      todo.classList.remove('d-flex')
+      todo.classList.add('hidden')
+    })
+  Array.from(todosContainer.children)
+  .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+  .forEach(todo => {
+    todo.classList.remove('hidden')
+    todo.classList.add('d-flex')
+  })
 })
