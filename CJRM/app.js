04d932c98817462109b80afb18fd29f575117362
Iury Chagas <iurychagas@icloud.com>
2021-04-17 18:56:43 -0300

    Add about 'Date' --JavaScript Global Object

--- CJRM/app.js
+++ CJRM/app.js
@@ -1,66 +1,24 @@
-const todosContainer = document.querySelector('.todos-container')
-const formAddTodo = document.querySelector('.form-add-todo')
-const inputSearchTodo = document.querySelector('.form-search input')
+const present = new Date()
 
-const addTodo = inputValue => {
-  if (inputValue.length) {
-    todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
-      <span>${inputValue}</span>
-      <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
-    </li>
-    `
-    event.target.reset()
-  }
-}
+console.log(present)
+console.log('Year: ', present.getFullYear())
+console.log('Month: ', present.getMonth(), '<-- Zer0 base')
+console.log('Day of Month: ', present.getDate())
+console.log('Day of Week: ', present.getDay(), ' "Sabado" <-- Zer0 base')
+console.log('Hour:', present.getHours())
+console.log('Minutes:', present.getMinutes())
+console.log('Seconds:', present.getSeconds())
+console.log('Milliseconds:', present.getMilliseconds())
 
-formAddTodo.addEventListener('submit', event => {
-  event.preventDefault()
 
-  const inputValue = event.target.add.value.trim()
+console.log(typeof present, present)
 
-  addTodo(inputValue)
-})
+const concatedDate = `${present.getDate()}/0${present.getMonth()}/${present.getFullYear()}`
 
-const removeTask = clickedElement => {
-  const trashDataValue = clickedElement.target.dataset.trash
-  const todo = document.querySelector(`[data-todo="${trashDataValue}"]`)
+console.log('Concated Date: ', concatedDate)
 
-  if (trashDataValue) {
-    todo.remove()
-  }
-}
+console.log('Timestamp:', present.getTime(), 'Milliseconds since 1970')
 
-const filterTasks = (tasks, inputValue, returnMatchedTasks) => tasks
-  .filter(task => {
-    const matchedTasks = task.textContent.toLowerCase().includes(inputValue)
-    return returnMatchedTasks ? matchedTasks : !matchedTasks
-  })
-
-const manipulateClasses = (tasks, classToAdd, classToRemove) => {
-  tasks.forEach(tasks => {
-    tasks.classList.remove(classToRemove)
-    tasks.classList.add(classToAdd)
-  })
-}
-
-const hideTasks = (inputValue, tasks) => {
-  const tasksToHide = filterTasks(tasks, inputValue, false)
-  manipulateClasses(tasksToHide, 'hidden', 'd-flex')
-}
-
-const showTasks = (inputValue, tasks) => {
-  const tasksToShow = filterTasks(tasks, inputValue, true)
-  manipulateClasses(tasksToShow, 'd-flex', 'hidden')
-}
-
-const filterByCharacter = event => {
-  const inputValue = event.target.value.trim().toLowerCase()
-  const tasks = Array.from(todosContainer.children)
-    
-  hideTasks(inputValue, tasks)
-  showTasks(inputValue, tasks)
-}
-
-todosContainer.addEventListener('click', removeTask)
-inputSearchTodo.addEventListener('input', filterByCharacter)
+console.log('toDateString: ', present.toDateString())
+console.log('toTimeString: ', present.toTimeString())
+console.log('toLocaleString: ', present.toLocaleString())
