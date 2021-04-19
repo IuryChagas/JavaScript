d8910329290b41346fe6f10324a6e43cee015ef4
Iury Chagas <iurychagas@icloud.com>
2021-04-19 14:44:44 -0300

    Add about date-fns lib

--- CJRM/app.js
+++ CJRM/app.js
@@ -17,3 +17,18 @@ const updateClock = () => {
 setInterval(updateClock, 1000)
 
 
+const present = new Date()
+
+// console.log(dateFns.isToday(present))
+
+console.log(dateFns.format(present, 'MMMM'))
+console.log(dateFns.format(present, 'YY'))
+console.log(dateFns.format(present, 'DD'))
+console.log(dateFns.format(present, 'Do'), `<-- Dia do mês!`)
+console.log(dateFns.format(present, 'DD-MMM-YYYY'))
+console.log(dateFns.format(present, 'DD/MM/YYYY'))
+
+
+const past = new Date('April 7 2019 17:00:00')
+
+console.log(dateFns.distanceInWords(present, past, { addSuffix: true }))
