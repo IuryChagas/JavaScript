aefc2ec25117c83ca11164112d708abee69fdc82
Iury Chagas <iurychagas@icloud.com>
2021-04-19 14:27:56 -0300

    Add Digital watch

--- CJRM/app.js
+++ CJRM/app.js
@@ -1,31 +1,19 @@
-const past = new Date('May 1 2020 7:47:00')
-const present = new Date()
+const clockContainer = document.querySelector('.clock-container')
 
-console.log('Timestamp:', present.getTime(), past)
-console.log('Timestamp:', present.getTime(), past.getTime())
-const difference = present.getTime() - past.getTime()
+const updateClock = () => {
+    const present = new Date()
+    const hours = present.getHours()
+    const minutes = present.getMinutes()
+    const seconds = present.getSeconds()
+    
+    const clockHTML = `
+        <span>${String(hours).length === 1 ? `0${hours}` : hours}</span> :
+        <span>${String(minutes).length === 1 ? `0${minutes}` : minutes}</span> :
+        <span>${String(seconds).length === 1 ? `0${seconds}` : seconds}</span>
+    `
+    clockContainer.innerHTML = clockHTML
+}
 
-console.log('Timestamp:', difference)
+setInterval(updateClock, 1000)
 
 
-const seconds = Math.round(difference / 1000)
-
-console.log({ seconds })
-
-const minutes = Math.round(seconds / 60)
-
-console.log({ minutes })
-
-const hours = Math.round(minutes / 60)
-
-console.log({ hours })
-
-const days = Math.round(hours / 24)
-
-console.log({ days })
-
-console.log(`Vídeo criado há ${days} dias`)
-
-const timestamp = 1618711221430
-
-console.log(new Date(timestamp))
