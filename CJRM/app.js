2fbcfccc33d9a6d9579baa4b550cbe05673fd7e3
Iury Chagas <iurychagas@icloud.com>
2021-04-19 21:58:49 -0300

    Add about Asynchronous code

--- CJRM/app.js
+++ CJRM/app.js
@@ -1,34 +1,15 @@
-const clockContainer = document.querySelector('.clock-container')
+console.log(1)
+console.log(2)
 
-const updateClock = () => {
-    const present = new Date()
-    const hours = present.getHours()
-    const minutes = present.getMinutes()
-    const seconds = present.getSeconds()
-    
-    const clockHTML = `
-        <span>${String(hours).length === 1 ? `0${hours}` : hours}</span> :
-        <span>${String(minutes).length === 1 ? `0${minutes}` : minutes}</span> :
-        <span>${String(seconds).length === 1 ? `0${seconds}` : seconds}</span>
-    `
-    clockContainer.innerHTML = clockHTML
-}
+setTimeout(()=>{
+    console.log('função de callback executada!')
+}, 2000)
 
-setInterval(updateClock, 1000)
+console.log(3)
+console.log(4)
 
 
-const present = new Date()
 
-// console.log(dateFns.isToday(present))
-
-console.log(dateFns.format(present, 'MMMM'))
-console.log(dateFns.format(present, 'YY'))
-console.log(dateFns.format(present, 'DD'))
-console.log(dateFns.format(present, 'Do'), `<-- Dia do mês!`)
-console.log(dateFns.format(present, 'DD-MMM-YYYY'))
-console.log(dateFns.format(present, 'DD/MM/YYYY'))
-
-
-const past = new Date('April 7 2019 17:00:00')
-
-console.log(dateFns.distanceInWords(present, past, { addSuffix: true }))
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then( response => response.json())
+    .then( json => console.log(json))
