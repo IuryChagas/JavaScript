310bd753f48207ec64a004ee308d243c0e375ac0
Iury Chagas <iurychagas@icloud.com>
2021-04-27 18:08:47 -0300

    Add about httpRequests and Callbacks

--- CJRM/app.js
+++ CJRM/app.js
@@ -1,13 +1,34 @@
-const request = new XMLHttpRequest()
+const getTodos = callback => {
+    const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', ()=> {
-    // console.log(request, request.readyState)
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request.responseText)
+    request.addEventListener('readystatechange', ()=>{
+        const requestSuccess = request.readyState === 4 && request.status === 200
+        const requestNotSuccess = request.readyState === 4
+
+        if (requestSuccess) {
+            callback(null, request.responseText)
+            return
+        }
+        
+        if (requestNotSuccess){
+            callback('Não foi possível obter os dados', null)
+        }
+    })
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
+    request.send()
+}
+console.log(1)
+console.log(2)
+console.log(3)
+
+getTodos((error, data) => {
+    if (error) {
+        console.log(error)
+        return
     }
+    console.log(data)
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-request.send()
-
-// console.log(request)
+console.log(4)
+console.log(5)
