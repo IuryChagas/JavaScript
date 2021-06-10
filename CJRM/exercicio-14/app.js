8f324dc5f71aa4eae375c8eddbe968ac6649194c
Iury Chagas <iurychagas@icloud.com>
2021-06-10 17:33:54 -0300

    Add chapter14 exercises 09/09

--- CJRM/exercicio-14/app.js
+++ CJRM/exercicio-14/app.js
@@ -1,32 +1,82 @@
+console.info(
+  `
+  \n########################### EXPLANATION: Intro to DOM ###########################
+  
+  `
+)
+
+const paragraph = document.querySelector('p')
+
+console.log(paragraph)
+
+console.log(paragraph.getAttribute('class'))
+
+paragraph.setAttribute('class', 'success')
+
+paragraph.setAttribute('style', 'color: red')
+
+paragraph.innerText += ' Texto inserido'
+
+const paragraphs = document.querySelectorAll('p')
+
+paragraphs.forEach((paragraph, index) => {
+  paragraph.innerText += ` Novo texto ${index +1}`
+} )
+
+const div = document.querySelector('.content')
+
+div.innerHTML = `<h2> Novo H2 </h2>`
+
+const people = ['Jessica', 'Karine', 'Pietra']
+
+people.forEach( person => {
+  div.innerHTML += `<p>${person}</p>`
+})
+
+const link = document.querySelector('a')
+// console.log(link.getAttribute('href'))
+
+link.setAttribute('href','https://twitter.com/IuryChagas_')
+
+link.innerText = 'Twitter do Iury Chagas'
+
+console.info(
+`
+\n###########################     EXERCISES CHAPTER 14    ###########################
+
+`
+)
 /*
   01
 
   - Faça com que o texto do h1 do index.html seja exibido com todas as letras  
     maiúsculas.
 */
-console.info('\n### Etapa 05 - Exercício: 01 ###\n ')
+console.info("## Exercise >> 01")
 
-const title = document.querySelector('h1')
+const h1 = document.querySelector('h1')
 
-const uperCaseTitle = title.textContent.toUpperCase()
-console.log(uperCaseTitle)
+h1.innerText = h1.innerText.toUpperCase()
 
+console.log(h1.textContent = h1.textContent.toUpperCase())
 /*
   02
 
   - Adicione, na ul vazia do index.html, os números do array "numbers" abaixo;
   - Cada número deve estar dentro de uma <li> com a classe "number".
 */
-console.info('\n### Etapa 05 - Exercício: 02 ###\n ')
+console.info("## Exercise >> 02")
+
 const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
 
-let ul = document.querySelector('ul')
+let ul = document.querySelector('.numbers')
+const insertNumberIntoHTML = number => {
+  ul.innerHTML += `<li class="number">${number}</li>`
+}
 
-numbers.forEach(num => {
-  ul.innerHTML += `<li class='number'>${num}</li>`
-})
+numbers.forEach(insertNumberIntoHTML)
 
-ul.classList.add('strong')
+console.log(ul)
 /*
   03
 
@@ -34,26 +84,23 @@ ul.classList.add('strong')
     - Se o número é par, ele deve ser exibido na cor "lightblue";
     - Se o número é ímpar, exiba-o na cor "pink".
 */
-console.info('\n### Etapa 05 - Exercício: 03 ###\n ')
+console.info("## Exercise >> 03")
 
-let list = document.querySelectorAll('li')
+const list = document.querySelectorAll('.number')
 
-const changeColor = numb => {
+const changeLiColor = li => {
+  const isEvenNumber = Number(li.textContent) % 2 === 0
 
-  let evenNumber = +numb.textContent % 2 === 0
-  let oddNumber = +numb.textContent % 2 !== 0
+  if (isEvenNumber) {
+    return li.style.color = 'lightblue'
+  }
 
-    if(evenNumber){
-      numb.classList.add('lightblue')
-    }
-    if(oddNumber){
-      numb.classList.add('pink')
-    }
-  console.log(numb)
+  li.style.color = 'pink'
 }
 
-list.forEach(changeColor)
+list.forEach(changeLiColor)
 
+console.log(list)
 /*
   04
 
@@ -61,36 +108,40 @@ list.forEach(changeColor)
 
   P.s: a classe "body-background" já está declarada no style.css.
 */
-console.info('\n### Etapa 05 - Exercício: 04 ###\n ')
+console.info("## Exercise >> 04")
 
-// let indexHTML = document.querySelector('body')
+const body = document.querySelector('body')
 
-let body = document.body
-body.classList.add('body-background')
+body.classList.add("body-background")
 
-console.log(body.getAttribute('class'))
+console.log(body)
 
 /*
   05
 
   - Adicione o link 
-    https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo no href  
+    https://github.com/IuryChagas/JavaScript/tree/master/CJRM no href  
     do link do index.html.
 */
-console.info('\n### Etapa 05 - Exercício: 05 ###\n ')
+console.info("## Exercise >> 05")
 
-let href = document.querySelector('a')
+const url = document.querySelector('a')
 
-href.setAttribute('href','https://github.com/IuryChagas/JavaScript/tree/master/CJRM')
+// url.href = 'https://github.com/IuryChagas/JavaScript/tree/master/CJRM'
+
+url.setAttribute('href', 'https://github.com/IuryChagas/JavaScript/tree/master/CJRM')
+url.setAttribute('class','link')
+url.textContent = 'Github:: IuryChagas'
 
 /*
   06
 
   - Exiba o novo valor do atributo href do link no console.
 */
-console.info('\n### Etapa 05 - Exercício: 06 ###\n ')
+console.info("## Exercise >> 06")
 
-console.log(href.getAttribute('href'))
+// console.log(url.href) or..
+console.log(url.getAttribute('href'))
 
 /*
   07
@@ -98,21 +149,21 @@ console.log(href.getAttribute('href'))
   - Exiba, no console, um objeto com todas as propriedades CSS que podem ser  
     manipuladas via JS no h1.
 */
-console.info('\n### Etapa 05 - Exercício: 07 ###\n ')
-
-let h1 = document.querySelector('h1')
+console.info("## Exercise >> 07")
 
 console.log(h1.style)
-
 
 /*
   08
 
   - Remova a classe "body-background", do elemento body.
 */
-console.info('\n### Etapa 05 - Exercício: 08 ###\n ')
+console.info("## Exercise >> 08")
+
+// body.setAttribute('class', '') or..
 
 body.classList.remove('body-background')
+console.log(' -[x] Remova a classe "body-background", do elemento body.')
 
 /*
   09
@@ -120,6 +171,8 @@ body.classList.remove('body-background')
   - Se o link da página possuir uma classe "link", remova-a;
   - Não utilize o método remove() para fazer isso.
 */
-console.info('\n### Etapa 05 - Exercício: 09 ###\n ')
+console.info("## Exercise >> 09")
 
-href.classList.toggle('link')
+// ul.classList.toggle('link')
+
+console.log('-[x] Se o link da página possuir uma classe "link", remova-a;')
