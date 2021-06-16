115a5608bf6b5e3e87bec3777e6e535013c31d52
Iury Chagas <iurychagas@icloud.com>
2021-06-15 22:18:01 -0300

    Add chapter18 exercises 02/03

--- CJRM/exercicio-18/app.js
+++ CJRM/exercicio-18/app.js
@@ -1,5 +1,12 @@
+console.info(
+  `
+  \n###########################     EXERCISES CHAPTER 18   ###########################
+
+  `
+)
+
 /*
-  Apenas 3 exercícios, mas que exigem um certo nível de conhecimento do que
+  Apenas 3 exercícios, mas que exigem um certo nível de conhecimento do que  
   vimos até aqui =)
 */
 
@@ -7,133 +14,89 @@
   01
 
   - Valide o valor do input "username" à medida em que ele é digitado;
-  - Ele deve conter:
+  - Ele deve conter: 
     - No mínimo 6 caracteres;
     - Apenas letras maiúsculas e/ou minúsculas;
-  - Se o valor inserido não é válido, exiba um parágrafo laranja abaixo do
-    input com a seguinte mensagem: "O valor deve conter no mínimo 6 caracteres,
+  - Se o valor inserido não é válido, exiba um parágrafo laranja abaixo do  
+    input com a seguinte mensagem: "O valor deve conter no mínimo 6 caracteres,  
     com apenas letras maiúsculas e/ou minúsculas";
-  - Se o valor é válido, o parágrafo deve ser verde e exibir a mensagem
+  - Se o valor é válido, o parágrafo deve ser verde e exibir a mensagem  
     "Username válido =)";
   - Use as classes disponíveis no arquivo style.css para colorir o parágrafo;
   - Não insira o parágrafo manualmente no index.html.
-
+  
   Dica: pesquise pelo método "insertAdjacentElement", no MDN;
 */
+console.info("## Exercise >> 01")
 
 const form = document.querySelector('form')
-const input = document.querySelector('#username')
-const button = document.querySelector('button')
-const paragraphUserNameFeedback = document.createElement('p')
-const paragraphSubmitFeedback = document.createElement('p')
+const inputUserName = document.querySelector('#username')
+const paragraph = document.createElement('p')
 
-paragraphSubmitFeedback.setAttribute('data-feedback', 'submit-feedback')
-
-const insertParagraphIntoDOM = paragraphInfo => {
-  const {paragraph, text, className, AdjacentElementRefer} = paragraphInfo
-
-  paragraph.setAttribute('class', className)
-  paragraph.textContent = text
-  AdjacentElementRefer.insertAdjacentElement('afterend', paragraph)
-}
-
-const invalidSubmitInfo = {
-  paragraph: paragraphSubmitFeedback,
-  text:'Por favor, insira um username válido',
-  className:'submit-help-feedback',
-  AdjacentElementRefer: button
-}
-
-const validSubmitinfo = {
-  paragraph: paragraphSubmitFeedback,
-  text: 'Dados enviados!',
-  className: 'submit-success-feedback',
-  AdjacentElementRefer: button
-}
-
-const invalidUserNameInfo = {
-  paragraph: paragraphUserNameFeedback,
-  text: 'O valor deve conter no mínimo 6 caracteres, com apenas letras maiúsculas e/ou minúsculas',
-  className: 'username-help-feedback',
-  AdjacentElementRefer: input
-}
-
-const validUserNameInfo = {
-  paragraph: paragraphUserNameFeedback,
-  text: 'Username válido!',
-  className: 'username-success-feedback',
-  AdjacentElementRefer: input
-}
-
-const removeSubmitParagraph = () => {
-
-  const cssSelectorReference = '[data-feedback="submit-feedback"]'
-  const paragraphSubmitFeedbackExists = document.querySelector(cssSelectorReference)
-
-  if (paragraphSubmitFeedbackExists) {
-    paragraphSubmitFeedbackExists.remove()
-  }
+const createParagraphMessage = (msg, styleFeedback) => {
+  paragraph.setAttribute('class', styleFeedback)
+  paragraph.textContent = msg
+  inputUserName.insertAdjacentElement('afterend', paragraph)
 
 }
+const validPattern = /^[a-zA-Z]{6,}$/
 
-const testUserName = inputValue => {
-  return /^[a-zA-Z]{6,}$/.test(inputValue)
+
+const feedBackSuccess = () => {
+  const msg = 'Username válido =)'
+  const styleCssSuccess = `username-success-feedback`
+  createParagraphMessage(msg, styleCssSuccess)
+}
+const feedBackError = () => {
+  const msg = 'O valor deve conter no mínimo 6 caracteres, com apenas letras maiúsculas e/ou minúsculas'
+  const styleCssError = `username-help-feedback`
+  createParagraphMessage(msg, styleCssError)
 }
 
-const showUserNameInfo = event => {
+inputUserName.addEventListener('input', event => {
+  const isValidPattern = validPattern.test(event.target.value)
 
-  const isUserNameValid = testUserName(event.target.value)
-
-  removeSubmitParagraph()
-
-  // ** ##### Outra forma de obter o mesmo resultado #####
-
-  event.target.insertAdjacentElement('afterend', paragraphUserNameFeedback)
-
-  if (!isUserNameValid) {
-
-    insertParagraphIntoDOM(invalidUserNameInfo)
+  if (isValidPattern) {
+    feedBackSuccess()
     return
   }
-
-  insertParagraphIntoDOM(validUserNameInfo)
-}
-
+  feedBackError()
+})
 /*
   02
 
   - Valide o envio do form;
-  - Se o username inserido no input é válido, no envio do form, exiba um
+  - Se o username inserido no input é válido, no envio do form, exiba um  
     parágrafo verde abaixo do botão com a mensagem "Dados enviados =)";
-  - Se no momento do envio, o valor do input é inválido, o parágrafo deve ser
+  - Se no momento do envio, o valor do input é inválido, o parágrafo deve ser  
     vermelho e exibir "Por favor, insira um username válido".
   - Use as classes disponíveis no arquivo style.css para colorir o parágrafo;
   - Não insira o parágrafo manualmente no index.html.
 */
 
-const showSubmitInfo = event => {
+const insertParagraph = (styleFeedback, message) => {
+  paragraph.setAttribute('class', styleFeedback)
+  paragraph.textContent = message
+}
+
+form.addEventListener('submit', event => {
   event.preventDefault()
-
-  const isUserNameValid = testUserName(input.value)
-
-  if (!isUserNameValid) {
-
-    insertParagraphIntoDOM(invalidSubmitInfo)
+ 
+  if(validPattern.test(inputUserName.value)){
+    const msg = 'Dados enviados =)'
+    insertParagraph('submit-success-feedback', msg)
     return
   }
 
-  insertParagraphIntoDOM(validSubmitinfo)
-}
-
-input.addEventListener('input', showUserNameInfo)
-form.addEventListener('submit', showSubmitInfo)
-
+  const msg = 'Por favor, insira um username válido'
+  insertParagraph('submit-help-feedback', msg)
+})
 /*
   03
 
   - Há algumas aulas, falamos sobre o método some;
   - Neste exercício, seu desafio será criar este método do zero;
-  - Implemente uma função "some" que possui a mesma funcionalidade do método
+  - Implemente uma função "some" que possui a mesma funcionalidade do método  
     some original;
   - A assinatura da invocação desta função deverá ser:
     - some([1, 2, 3], item => item > 2) - Retorna true;
@@ -141,31 +104,10 @@ form.addEventListener('submit', showSubmitInfo)
   - Se você não se lembra como o método some funciona, há 2 opções:
     1) Reassistir às seguintes aulas:
       - "Desenvolvendo um popup" - Aula 04-04 da etapa 5;
-      - "Correção dos exercícios da aula 04 da etapa 05" - Aula 01-01 da etapa 6;
+      - "Correção dos exercícios da aula 04 da etapa 05" - Aula 01-01 da etapa  
+        6;
     2) Pesquisar no MDN.
-
+  
   Spoiler alert: este tipo de exercício será frequente em etapas mais avançadas  
   do curso, onde falaremos sobre TDD. Vá se aquecendo =D
 */
-
-const some = (arr, func) => {
-  for (let index = 0; index < arr.length; index++) {
-    if (func(arr[index])) {
-        return true
-    }
-  }
-  return false
-}
-
-console.log(some([0,1,2,3,4], item => item === 2))
-console.log(some([5,6,7,8,9], item => item === 19))
-
-
-  // **
-
-  // paragraphUserNameFeedback.setAttribute('data-feedback', 'username-feedback')
-  // const feedbackParagraph = document.querySelector('[data-feedback="username-feedback"]')
-
-  // if (feedbackParagraph) {
-  //   feedbackParagraph.remove()
-  // }
