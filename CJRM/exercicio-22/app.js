941f180cb1e520250e16f9312b651adecaca1b09
Iury Chagas <iurychagas@gmail.com>
2021-06-30 12:06:07 -0300

    Add chapter22 exercises 07/10

--- CJRM/exercicio-22/app.js
+++ CJRM/exercicio-22/app.js
@@ -18,3 +18,196 @@ const booksOnSale = books
 
 console.table(booksOnSale)
 
+/*
+  01
+
+  - Ordene o array de strings abaixo em ordem alfabética;
+  - Não modifique o array original;
+  - Exiba o array ordenado no console.
+*/
+console.info("## Exercise >> 01")
+
+const names = ['Caio', 'André', 'Dário']
+
+const namesOrdenated = names.map( name => name ).sort()
+
+console.table(namesOrdenated)
+console.table(names)
+
+/*
+  02
+
+  - Ordene, de forma crescente, os objetos do array abaixo baseado em seus id's;
+  - Não modifique o array original;
+  - Exiba o array ordenado no console.
+*/
+console.info("## Exercise >> 02")
+
+const characters = [
+  { id: 03, name: 'Simba' },
+  { id: 02, name: 'Nala' },
+  { id: 01, name: 'Scar' },
+  { id: 04, name: 'Mufasa' }
+]
+
+const charactersOrdenated = characters.map( char => ({ id: char.id, name: char.name }))
+
+charactersOrdenated.sort( (char1, char2) => char1.id - char2.id)
+
+console.table(charactersOrdenated)
+console.table(characters)
+/*
+  03
+
+  - Ordene o array de números abaixo de forma crescente;
+  - Não modifique o array original;
+  - Exiba o array ordenado no console.
+*/
+console.info("## Exercise >> 03")
+
+const numbers = [41, 15, 63, 349, 25, 22, 143, 64, 59, 291]
+
+numbersOrdenated = numbers.map( number => number )
+
+numbersOrdenated.sort((number1, number2) => number1 - number2)
+
+console.log(numbersOrdenated)
+
+/*
+  04
+
+  - Encontre e exiba no console o 1º item maior que 50 do array abaixo.
+*/
+console.info("## Exercise >> 04")
+
+const randomNumbers = [10, 5, 0, 40, 60, 10, 20, 70]
+
+const firstNumberGreaterThan50 = randomNumbers.find( number => number > 50)
+
+console.log(firstNumberGreaterThan50)
+/*
+  05
+
+  - Ordene o array de strings abaixo em ordem alfabética-invertida (Z-A);
+  - Não modifique o array original;
+  - Exiba o array ordenado no console.
+*/
+console.info("## Exercise >> 05")
+
+const people = ['Cauã', 'Alfredo', 'Bruno']
+
+const invertedOrdenate = people.map( person => person )
+
+invertedOrdenate.sort().reverse()
+
+console.table({ people, invertedOrdenate })
+
+/*
+  06
+  
+  - Através do array abaixo, gere a mensagem "vinho cozido, tomate cozido, 
+    cebola cozida, cogumelo cozido";
+  - Exiba a string no console.
+*/
+console.info("## Exercise >> 06")
+
+const ingredients = ['vinho', 'tomate', 'cebola', 'cogumelo']
+
+const phrase = ingredients.reduce((string, ingredient, index, arr) => {
+  // const correctWordGender = /a$/.test(ingredient) ? 'cozida' : 'cozido'
+  const genderClassification = ingredient[ingredient.length - 1] === 'a' ? 'cozida' : 'cozido'
+  
+  if (index === arr.length -1) {
+    return string + `${ingredient} ${genderClassification}` 
+  }
+  return string + `${ingredient} ${genderClassification}, `
+}, ``)
+
+console.log(phrase)
+/*
+  07
+  
+  - À partir do array abaixo, obtenha e exiba no console o total de pessoas que 
+    assistiram apenas os filmes da Disney.
+*/
+console.info("## Exercise >> 07")
+
+const topBrazilmovies = [
+  { title: 'Vingadores: Ultimato', peopleAmount: 19686119, distributedBy: 'Disney' },
+  { title: 'Titanic', peopleAmount: 17050000, distributedBy: 'Paramount / 20th Century' },
+  { title: 'O Rei Leão', peopleAmount: 16267649, distributedBy: 'Disney' },
+  { title: 'Vingadores: Guerra Infinita', peopleAmount: 14572181, distributedBy: 'Disney' },
+  { title: 'Tubarão', peopleAmount: 13035000, distributedBy: 'Universal' },
+  { title: 'Nada a Perder', peopleAmount: 11944985, distributedBy: 'Paris Filmes' },
+  { title: 'Os Dez Mandamentos', peopleAmount: 11259536, distributedBy: 'Paris / Downtown Filmes' },
+  { title: 'Tropa de Elite 2', peopleAmount: 11204815, distributedBy: 'Zazen' },
+  { title: 'Os Vingadores', peopleAmount: 10968065, distributedBy: 'Disney' },
+  { title: 'Dona Flor e Seus Dois Maridos', peopleAmount: 10735524, distributedBy: 'Embrafilme' }
+]
+
+const moviesSelected = topBrazilmovies
+  .filter(({ distributedBy }) => distributedBy === 'Disney')
+  .reduce((total, { peopleAmount }) => total += peopleAmount, 0)
+
+console.log(moviesSelected)
+
+/*
+  08
+  
+  - Considerando o array abaixo, gere um array de cães;
+  - Os cães, ao invés da idade original, devem conter sua "idade humana";
+    - Algumas pessoas dizem que 1 ano de um cachorro equivale à 7 anos de uma 
+      pessoa. Cientificamente, não é tão simples assim, mas para fins didáticos,  
+      se baseie nessa informação para fazer o cálculo.
+  - Exiba no console o array dos cães com as idades convertidas.
+*/
+console.info("## Exercise >> 08")
+
+const pets = [
+  { name: 'Boris', age: 4, gender: 'Male', type: 'Dog' },
+  { name: 'Jimmy', age: 1, gender: 'Male', type: 'Cat' },
+  { name: 'Pérola', age: 2, gender: 'Female', type: 'Dog' },
+  { name: 'Lucy', age: 5, gender: 'Female', type: 'Cat' },
+  { name: 'Cristal', age: 3, gender: 'Female', type: 'Dog' },
+  { name: 'Chico', age: 6, gender: 'Male', type: 'Dog' }
+]
+
+const dogs = pets.filter( ({ type }) => type === 'Dog').map( dog =>  ({
+    name: dog.name,
+    age: dog.age * 7,
+    gender: dog.gender,
+    type: dog.type
+}))
+
+console.table(dogs)
+/*
+  09
+  
+  - Considerando o array topBrazilmovies, através do map ou do reduce, insira 
+    os nomes dos filmes na ul do index.html.
+*/
+console.info("## Exercise >> 09")
+const ul = document.querySelector('.list-group')
+
+let templateHTML = ``
+
+topBrazilmovies.map( ({ title })=> {
+  templateHTML += `<li>${title}</li>`
+})
+
+ul.innerHTML = templateHTML
+console.log(templateHTML)
+
+/*
+  10
+  
+  - Na sua versão do Quiz, teste se ao enviar o form pela 2ª vez consecutiva, a 
+    pontuação do envio anterior é contabilizada;
+  - Exemplo: exibe 100% no 1º envio e 200% no 2º envio (sem modificar as 
+    alternativas entre os envios);
+    - Se isso está acontecendo, proponha uma solução para que o 2º submit não 
+      considere a pontuação do envio anterior.
+*/
+
+console.info("## Exercise >> 10")
+
