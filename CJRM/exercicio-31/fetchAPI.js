e55e3f2d405aab105d697acf39157fc5cb9f85bf
Iury Chagas <iurychagas@icloud.com>
2021-07-08 11:17:57 -0300

    Add chapter31 - fetch API x Async/Await

--- CJRM/exercicio-31/fetchAPI.js
+++ CJRM/exercicio-31/fetchAPI.js
@@ -1,3 +1,7 @@
+setTimeout( ()=> {
+    console.info("########### Fetch API - Promises()  ###########")
+}, 10)
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then( response => {
         console.log(response)
@@ -7,3 +11,21 @@ fetch('https://jsonplaceholder.typicode.com/users')
         console.table(users)
     })
     .catch( error => console.log(error))
+
+
+setTimeout( ()=> {
+    console.info("########### Async/Await  - Promises() ###########")
+}, 100)
+
+
+const getUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    return await response.json()
+}
+
+const logUsers = async () => {
+    const users = await getUsers()
+    console.table(users)
+}
+
+logUsers()
