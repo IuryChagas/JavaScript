cba014b55923a91ff333957b60eed9ed350cd1be
Iury Chagas <iurychagas@icloud.com>
2021-07-08 23:00:06 -0300

    Add chapter32 - parallel requests

--- CJRM/exercicio-32/sequential_vs_parallel_requests.js
+++ CJRM/exercicio-32/sequential_vs_parallel_requests.js
@@ -1,12 +1,12 @@
 const getPokemons = async () => {
-    const bulbasaur = await fetch('https://pokeapi.co/api/v2/pokemon/1')
-    const charmander = await fetch('https://pokeapi.co/api/v2/pokemon/4')
-    const squirtle = await  fetch('https://pokeapi.co/api/v2/pokemon/7')
+    const bulbasaur = fetch('https://pokeapi.co/api/v2/pokemon/1')
+    const charmander = fetch('https://pokeapi.co/api/v2/pokemon/4')
+    const squirtle = fetch('https://pokeapi.co/api/v2/pokemon/7')
 
-    console.log(await bulbasaur.json())
-    console.log(await charmander.json())
-    console.log(await squirtle.json())
-
+    const results = await Promise.all([bulbasaur, charmander, squirtle])
+    results.forEach( async response => {
+        return console.log( await response.json() )
+    })
 }
 
 getPokemons()
