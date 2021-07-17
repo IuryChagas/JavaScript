8202789d620432cd458c01f2d83b75d3c055f462
Iury Chagas <iurychagas@icloud.com>
2021-07-17 11:17:39 -0300

    Add chapter32 - refactoring a fetch request --stage 01

--- CJRM/exercicio-32/fetching-request.js
+++ CJRM/exercicio-32/fetching-request.js
@@ -1,6 +1,6 @@
 const getUsers = async ()=> {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    return await response.json()
+    const users = await fetch('https://jsonplaceholder.typicode.com/users').then( response => response.json() )
+    return users
 }
 
 const logUsers = async ()=> {
