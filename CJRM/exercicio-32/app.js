3680ac571411cf6854748ea95afeb069e8e75f28
Iury Chagas <iurychagas@icloud.com>
2021-07-18 14:58:17 -0300

    Add chapter32 exercise 01/01

---
+++ CJRM/exercicio-32/app.js
@@ -0,0 +1,79 @@
+console.info(
+  `
+  \n###########################   EXERCISES CHAPTER 32   ###########################
+
+  `
+)
+
+/*
+  Neste exercício, vamos praticar um pouco do que vimos até aqui, através da 
+  API do GIPHY, que é um website de GIFs (https://giphy.com/).
+
+  - Seu desafio é implementar uma funcionalidade de busca dos GIFs. Quando o 
+    usuário digitar o termo de busca no input e pressionar enter, um GIF deve 
+    ser exibido na tela. Como neste exemplo: https://youtu.be/RHe-uCJGCeA
+  - Observe que o GIF mais recente é inserido acima dos GIFs anteriores;
+  - Para fazer requests para a API do GIPHY, você precisará de uma API key. 
+    Para obtê-la, siga os seguintes passos:
+    - Acesse https://developers.giphy.com/dashboard/ e faça o login;
+    - No Dashboard, clique em "Create an App", clique em "API Selected" e em 
+      "Next Step";
+    - Dê um nome e descrição para o app e crie-o;
+    - Clique no código da API key para copiá-la;
+  - O submit do form deve ser feito para o endpoint abaixo. Atente-se para os 2
+    [PLACEHOLDERS] que devem ser substituídos:
+    - https://api.giphy.com/v1/gifs/search?api_key=[SUA_CHAVE_DA_API_AQUI]&limit=1&q=[VALOR_INSERIDO_NO_INPUT]
+    - Se quiser testar outras possibilidades, os endpoints da API estão
+      listados na documentação: https://developers.giphy.com/docs/api/endpoint#search
+  - Ignore os avisos no console. Para limpá-lo, pressione "ctrl + L".
+*/
+
+const form = document.querySelector('form')
+const gifsContainer = document.querySelector('.contents')
+const giphyApiKey = `tEt0n9T7FWAJhCmn1Ux9N7kQgf9kyoyS`
+
+const setEndPointURL = (apiKey, gifName) => {
+  const endPoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${gifName}&limit=1&offset=0&rating=g&lang=en`
+  return endPoint
+}
+
+const setImageGif = (url, alt, title) => {
+  const img = document.createElement('img')
+
+  img.setAttribute('src', url)
+  img.setAttribute('alt', alt)
+  img.setAttribute('title', title)
+  
+  return img
+}
+
+const insertGifAtContentContainer = (position, img) => {
+  return gifsContainer.insertAdjacentElement(position, img)
+}
+
+form.addEventListener('submit', async event => {
+  event.preventDefault()
+  const inputValue = event.target.search.value
+
+  const endPoint = setEndPointURL(giphyApiKey, inputValue)
+
+  try {
+    const response = await fetch(endPoint)
+    
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    const gifDataObject = await response.json()
+    const gifURL = gifDataObject.data[0].images.downsized.url
+    const gifTitle = gifDataObject.data[0].title
+
+    const img = setImageGif(gifURL, gifTitle, gifTitle)
+    insertGifAtContentContainer('afterbegin', img)
+
+  } catch (error) {
+    console.error(error)
+  }
+
+  event.target.reset()
+})
