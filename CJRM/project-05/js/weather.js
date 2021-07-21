224579c7f5956f99b53b6a85f79859f7cbf6cba0
Iury Chagas <iurychagas@icloud.com>
2021-07-21 01:17:52 -0300

    Add fifth project --fetch functions refactoring

--- CJRM/project-05/js/weather.js
+++ CJRM/project-05/js/weather.js
@@ -1,50 +1,33 @@
 const APIKey = 'MjzrtGtGJxZm33v4B4uFAmAKG7znW1KX'
 const languageAcronym = 'pt-br'
+const baseURL = `http://dataservice.accuweather.com/`
 
-const getCityUrl = cityName => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+const getCityUrl = cityName => `${baseURL}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+const getCityWeatherURL = ({ Key }) => `${baseURL}currentconditions/v1/${Key}?apikey=${APIKey}&language=${languageAcronym}`
 
-const getCityKey = async cityName => {
-
-    const endPoint = getCityUrl(cityName)
-
-    try {
-        const response = await fetch(endPoint)
-
-        if (!response.ok) {
-            throw new Error('Não foi possível obter os dados')
-        }
-
-        const [ cityData ] = await response.json()
-        console.log(cityData)
-        return cityData
-
-    } catch ({ name, message }) {
-
-        alert(`${name}: ${message}`)
-
-    }
-}
-
-const getCityWeather = async cityId  => {
-
-    const { Key } = await getCityKey(cityId)
-
-    const endPoint = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=${languageAcronym}`
-
+const fetchData = async endPoint => {
     try{
         const response = await fetch(endPoint)
 
         if (!response.ok) {
-
             throw new Error('Não foi possível obter os dados')
         }
 
-        const  [ cityWeatherData ] = await response.json()
-        return console.log(cityWeatherData)
+        return await response.json()
 
-    } catch ({ name, message }) {
-        alert(`${name}: ${message}`)
+    }catch (error) {
+        return console.erro(error)
     }
 }
 
-getCityWeather('Curitiba')
+const getCityData = cityName => fetchData( getCityUrl(cityName) )
+
+const getCityWeather = async cityName => {
+    const [ cityData ] = await getCityData(cityName)
+
+    const endPoint = getCityWeatherURL(cityData)
+
+    return fetchData(endPoint)
+}
+
+getCityWeather('Curitiba').then( console.log )
