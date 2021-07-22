749049851d5179d1aae5d7d9dd2fc55e26c895a5
Iury Chagas <iurychagas@icloud.com>
2021-07-22 00:07:32 -0300

    Add fifth project - Data insert on screen --second refactoring

--- CJRM/project-05/js/app.js
+++ CJRM/project-05/js/app.js
@@ -6,30 +6,36 @@ const cityNameContainer = document.querySelector('[data-js="city-name"]')
 const cityWeatherContainer = document.querySelector('[data-js="city-weather"')
 const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
 
-form.addEventListener('submit', async event => {
-    event.preventDefault()
+const showCityCard = () => {
+    return cityCard.classList.contains('d-none') ? cityCard.classList.remove('d-none') : false
+}
+const showPeriodDay = IsDayTime => {
+    return timeImg.src = IsDayTime ? './src/day.svg' : './src/night.svg'
+}
+const showCityWeatherInfo = async cityName => {
 
-    const inputValue = event.target.city.value
-
-    const [{ Key, LocalizedName }] = await getCityData(inputValue)
+    const [{ Key, LocalizedName }] = await getCityData(cityName)
     const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
 
-    if (cityCard.classList.contains('d-none')) {
-        cityCard.classList.remove('d-none')
-    }
-
-    IsDayTime ? timeImg.src  = './src/day.svg' : timeImg.src = './src/night.svg'
-
-    const weatherIcon = WeatherIcon
+    showPeriodDay(IsDayTime)
 
     const imageIcon = document.createElement('img')
-    imageIcon.setAttribute('src', `./src/icons/svg/${weatherIcon}.svg`)
+    imageIcon.setAttribute('src', `./src/icons/svg/${WeatherIcon}.svg`)
     imageIcon.setAttribute('class', 'time-icon')
     timeIcon.insertAdjacentElement('afterend', imageIcon)
 
     cityNameContainer.textContent = LocalizedName
     cityWeatherContainer.textContent = WeatherText
     cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const inputValue = event.target.city.value
+
+    showCityCard()
+    showCityWeatherInfo(inputValue)
 
     form.reset()
 })
