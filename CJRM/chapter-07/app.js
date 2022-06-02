f2edc432e325d12e8928a47c388051eca24a639e
Iury Chagas <iurychagas@icloud.com>
2022-06-02 20:52:37 -0300

    refact(chapter07): add code readability

--- CJRM/chapter-07/app.js
+++ CJRM/chapter-07/app.js
@@ -1,6 +1,6 @@
 console.info(
     `
-    \n:::::::::::::::::::::::::::   EXERCISES CHAPTER 06   :::::::::::::::::::::::::::
+    \n:::::::::::::::::::::::::::   EXERCISES CHAPTER 07   :::::::::::::::::::::::::::
     `
   )
 
@@ -15,40 +15,36 @@ const result = result => {
 /*
   01
 
-  - Utilizando function declaration, implemente uma função que recebe 2 números  
+  - Utilizando function declaration, implemente uma função que recebe 2 números
     por parâmetro e retorna o resultado da multiplicação entre esses 2 números;
   - Previna que esses parâmetros recebam undefined;
   - Exiba o resultado no console, sem inserir um console.log() dentro da função.
 */
 section(1)
 
-function multiplicator(num1 = 0, num2 = 0) {
-    return num1 * num2
+function multiply(firstNumber = 0, secondNumber = 0) {
+    return firstNumber * secondNumber
 }
 
-const multiplication = multiplicator(4, 5)
-
-result(multiplication)
+result(multiply(4, 5))
 /*
   02
 
-  - Faça o mesmo que o exercício acima pede, mas desta vez, implemente uma  
-    **function expression** que retorne o resultado da **divisão** entre esses  
-    2 números.
+  - Faça o mesmo que o exercício acima pede, mas desta vez, implemente uma
+    **function expression** que retorne o resultado da **divisão** entre esses 2 números.
 */
 section(2)
 
-const divisitor = (num1 = 0, num2 = 0) => {
-    return num1 / num2
+const divide = function (firstNumber = 0, secondNumber = 0) {
+    return firstNumber / secondNumber
 }
 
-result(divisitor(20, 2))
+result(divide(20, 2))
 
 /*
   03
 
-  - Implemente uma função que apenas exibe no console o valor recebido por  
-    parâmetro;
+  - Implemente uma função que apenas exibe no console o valor recebido por parâmetro;
   - Previna que o parâmetro dessa função receba undefined;
   - Faça a string abaixo ser exibida 7x no console;
   - A cada exibição, substitua o "X" pela informação correta;
@@ -58,13 +54,15 @@ result(divisitor(20, 2))
 */
 section(3)
 
-function logMessage ( message = null ) {
-    for(let i = 1; i <= 5; i++){
-        console.log(`{ ${message} }: " Esta é a ${i}ª vez que essa string é exibida."`)
-    }
+function logMessage ( message = `Um valor como argumento é obrigatório`) {
+  console.log(message)
 }
 
-logMessage('yeahh!')
+
+for(let i = 1; i <= 5; i++){
+  let count = i
+  logMessage(`Esta é a ${count}ª vez que essa string é exibida.`)
+}
 
 /*
   04
@@ -77,26 +75,26 @@ section(4)
 
 const millennialWords = ['lol', 'yolo', 'troll', 'stalkear', 'selfie', 'influencer', 'crush', 'fitness', 'hater', 'bae', 'random', 'kawaii', 'outfit', 'mood', 'fail']
 
-
-
-function upperCaseWords(arrayOfWords) {
+function transformToUppercase(arrayOfWords = []) {
   const upperCaseWords = []
 
   for (let i = 0; i < arrayOfWords.length; i++) {
-    const word = arrayOfWords[i]
-    upperCaseWords.push(word.toUpperCase())
+    const wordInUpperCase = arrayOfWords[i].toUpperCase()
+    upperCaseWords.push(wordInUpperCase)
   }
 
   return upperCaseWords
 }
 
-result(upperCaseWords(millennialWords))
+const millennialWordsInUpperCase = transformToUppercase(millennialWords)
+
+result(millennialWordsInUpperCase)
+
 /*
   05
 
   - Implemente uma função que retorna se um número é positivo;
-  - Use essa função para descobrir quantos números positivos o array 
-    "randomNumbers" possui;
+  - Use essa função para descobrir quantos números positivos o array "randomNumbers" possui;
   - Exiba a frase abaixo no console, inserindo as informações corretas.
 
   "O array "randomNumbers" possui XX números, sendo XX positivos e XX negativos."
@@ -106,24 +104,24 @@ section(5)
 const randomNumbers = [-2, 93, 34, -1, 1, 93, 11, -7, 47, -3]
 
 let amontNumbers = 0
-let evenNumbers = 0
-let oddNumbers = 0
+let positiveNumbers = 0
+let negativeNumbers = 0
 
-function classifyNumbers (arrayOfNumbers) {
+function classifyNumbers (arrayOfNumbers = []) {
   for (let i = 0; i < arrayOfNumbers.length; i++) {
     const number = arrayOfNumbers[i];
-    const isAEvenNumber = number % 2 === 0
+    const isPositiveNumber = number >= 1
 
-    if(isAEvenNumber){
-      evenNumbers++
+    if(isPositiveNumber){
+      positiveNumbers++
     }else {
-      oddNumbers++
+      negativeNumbers++
     }
 
     amontNumbers++
   }
 
-  return `O array "randomNumbers" possui ${amontNumbers} números, sendo ${evenNumbers} positivos e ${oddNumbers} negativos.`
+  return `O array "randomNumbers" possui ${amontNumbers} números, sendo ${positiveNumbers} positivos e ${negativeNumbers} negativos.`
 }
 
 result(classifyNumbers(randomNumbers))
@@ -132,14 +130,12 @@ result(classifyNumbers(randomNumbers))
   06
 
   - Descomente a invocação da função abaixo e implemente-a;
-  - Ela deve retornar um novo array com apenas os números ímpares do array 
-    passado por argumento;
-  - Exiba o novo array no console, sem inserir um console.log() dentro da  
-    função.
+  - Ela deve retornar um novo array com apenas os números ímpares do array passado por argumento;
+  - Exiba o novo array no console, sem inserir um console.log() dentro da função.
 */
 section(6)
 
-function getOddNumbers (arrayOfNumbers) {
+function getOddNumbers (arrayOfNumbers = []) {
   const oddNumbers = []
 
   for(let i = 0; i < arrayOfNumbers.length; i++){
@@ -152,9 +148,10 @@ function getOddNumbers (arrayOfNumbers) {
   return oddNumbers
 }
 
-const response = getOddNumbers([83, 52, 31, 73, 98, 37, 61, 56, 12, 24, 35, 3, 34, 80, 42])
+const oddNumbers = getOddNumbers([83, 52, 31, 73, 98, 37, 61, 56, 12, 24, 35, 3, 34, 80, 42])
 
-result(response)
+result(oddNumbers)
+
 /*
   07
 
