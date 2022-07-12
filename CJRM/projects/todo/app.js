80ffc57ca361c300920453ad770b571ee2bc2ff4
Iury Chagas <iurychagas@icloud.com>
2022-07-11 23:37:31 -0300

    refact(project/todo): add code readability
    code refactoring as exercises 03, 04 of chapter 23

--- CJRM/projects/todo/app.js
+++ CJRM/projects/todo/app.js
@@ -2,26 +2,36 @@ const todosContainer = document.querySelector('.todos-container')
 const formAddTodo = document.querySelector('.form-add-todo')
 const removeTodo = document.querySelectorAll('.delete')
 
-formAddTodo.addEventListener('submit', event => {
-    event.preventDefault()
+const addTodoTask = (event) => {
     const inputValue = event.target.add.value.trim()
+    const todoTaskTemplate = `
+    <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+     <span>${inputValue}</span>
+     <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+    </li>
+    `
 
     if (inputValue) {
-        todosContainer.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-         <span>${inputValue}</span>
-         <i class="far fa-trash-alt delete"></i>
-        </li>
-        `
+        todosContainer.innerHTML += todoTaskTemplate 
         event.target.reset()
     }
+}
+
+formAddTodo.addEventListener('submit', event => {
+    event.preventDefault()
+    addTodoTask(event)
 })
+
+const removeTodoTask = clickedElement => {
+    const trashDataValue = clickedElement.dataset.trash
+    const getTodoTask = document.querySelector(`[data-todo="${trashDataValue}"]`)
+
+    if(trashDataValue){
+        getTodoTask.remove()
+    }
+}
 
 todosContainer.addEventListener('click', event => {
     const clickedElement = event.target
-    const convertDomTokenListInArray = Array.from(clickedElement.classList)
-    const hasCssClassDelete = convertDomTokenListInArray.includes('delete')
-    const removeTask = clickedElement.parentElement.remove()
-
-    if(hasCssClassDelete) { removeTask }
+    removeTodoTask(clickedElement)
 })
